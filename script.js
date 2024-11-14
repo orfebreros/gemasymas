37fe72736ee922e76144938289bb0eabad6074a9
Leví <walterlevi98@gmail.com>
2024-11-13 18:00:25 -0600

    ultimos cambios

---
+++ script.js
@@ -0,0 +1,3 @@
+const fecha = new Date();
+var footer = "Copyright © " + fecha.getFullYear() + " Grupo Orfebreros, S.A. All Rights Reserved.";
+document.getElementById("DevuelveTextoFooterConAnio").innerHTML = footer;
